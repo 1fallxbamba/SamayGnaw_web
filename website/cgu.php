fcac05a7d53f638d950f3d41ad9f02bc80b7fe81
bamba2001 <cabathj0@gmail.com>
2021-06-15 11:12:39 +0000

    cup'finish the website before the night

---
+++ website/cgu.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link href="css2/styles.css" rel="stylesheet" />
+
+    <title>Conditions générales & termes d'utilisations</title>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="index.html">
+                <p>TOUKKI</p>
+            </a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars ml-1"></i>
+            </button>
+        </div>
+    </nav>
+    <br><br><br>
+    <div class="container art shadow-lg p-3 mb-5 rounded">
+        <br><br>
+        <div class="jumbotron">
+            <h1 class="display-4 text-center">Conditions générales & termes d'utilisations</h1>
+            <br>
+            <article>
+                <h3>Article 1 : Objet</h3>
+                <p>Les présentes CGU ou Conditions Générales d’Utilisation encadrent juridiquement l’utilisation des services de l’application Toukki
+                    Constituant le contrat entre la société Toukki, l’Utilisateur, l’accès au site doit être précédé de l’acceptation de ces CGU. L’accès à cette plateforme signifie l’acceptation des présentes CGU.
+                </p>
+            </article>
+
+            <article>
+                <h3>Article 2 : Mentions légales</h3>
+                <p>
+                    La plateforme est assurée par la société SOA, dont le siège social est localisé au Point-E rue de Kaolack, Dakar Sénégal.
+                    L’hébergeur du site toukki est la société planète hoster, sise au 5 Rue Keller, 59100 Roubaix, France.
+                </p>
+            </article>
+
+            <article>
+                <h3>Article 3 : Accès au site</h3>
+                <p>
+                    La plateforme permet d’accéder gratuitement aux services suivants :
+                    <ul>
+                        <li>Faire du Co-voiturage</li>
+                        <li>Consulter les trajets disponibles</li>
+                        <li>Faire des profits</li>
+                    </ul>
+                    Toukki est accessible gratuitement depuis n’importe où par tout utilisateur disposant d’un accès à Internet. Tous les frais nécessaires pour l’accès aux services (matériel informatique, connexion Internet…) sont à la charge de l’utilisateur.
+                    L’accès aux services dédiés aux membres s’effectue à l’aide d’un identifiant et d’un mot de passe.
+                    Pour des raisons de maintenance ou autres, l’accès au site peut être interrompu ou suspendu par l’éditeur sans préavis ni justification.
+                </p>
+            </article>
+
+            <article>
+                <h3>Article 4 : Collecte des données</h3>
+                <p>
+                    Pour la création du compte de l’Utilisateur, la collecte des informations au moment de l’inscription sur le site est nécessaire et obligatoire. Conformément à la loi n°78-17 du 6 janvier relative à l’informatique, aux fichiers et aux libertés, la collecte et le traitement d’informations personnelles s’effectuent dans le respect de la vie privée.
+                    Suivant la loi Informatique et Libertés en date du 6 janvier 1978, articles 39 et 40, l’Utilisateur dispose du droit d’accéder, de rectifier, de supprimer et d’opposer ses données personnelles. L’exercice de ce droit s’effectue par :
+                    <ul>
+                        <li>Le formulaire de contact</li>
+                        <li>Son espace client</li>
+                    </ul>
+                </p>
+            </article>
+
+            <article>
+                <h3>Article 5 : Propriété intellectuelle</h3>
+                <p>
+                    Les marques, logos ainsi que les contenus de Toukki(illustrations graphiques, textes…) sont protégés par le Code de la propriété intellectuelle et par le droit d’auteur.
+                    La reproduction et la copie des contenus par l’Utilisateur requièrent une autorisation préalable du site. Dans ce cas, toute utilisation à des usages commerciaux ou à des fins publicitaires est proscrite
+                </p>
+            </article>
+
+            <article>
+                <h3>Article 6 : Responsabilité</h3>
+                <p>
+                    Bien que les informations publiées sur le site soient réputées fiables, la plateforme se réserve la faculté d’une non-garantie de la fiabilité des sources.
+                    Les informations diffusées sur l’application présentées à titre purement informatif et sont sans valeur contractuelle. En dépit des mises à jour régulières, la responsabilité du site ne peut être engagée en cas de modification des dispositions administratives et juridiques apparaissant après la publication. Il en est de même pour l’utilisation et l’interprétation des informations communiquées sur la plateforme.
+                    Le site décline toute responsabilité concernant les éventuels virus pouvant infecter le matériel informatique de l’Utilisateur après l’utilisation ou l’accès à ce site.
+                    Le site ne peut être tenu pour responsable en cas de force majeure ou du fait imprévisible et insurmontable d’un tiers.
+                    La garantie totale de la sécurité et la confidentialité des données n’est pas assurée par le site. Cependant, le site s’engage à mettre en œuvre toutes les méthodes requises pour le faire au mieux.
+                </p>
+            </article>
+
+            <article>
+                <h3>Article 7 : Liens hypertextes</h3>
+                <p>
+                    Le site peut être constitué de liens hypertextes. En cliquant sur ces derniers, l’Utilisateur sortira de la plateforme. Cette dernière n’a pas de contrôle et ne peut pas être tenue responsable du contenu des pages web relatives à ces liens.
+                </p>
+            </article>
+
+            <article>
+                <h3>Article 8 : Cookies</h3>
+                <p>
+                    Lors des visites sur l’application, l’installation automatique d’un cookie sur le logiciel de navigation de l’Utilisateur peut survenir.
+                    Les cookies correspondent à de petits fichiers déposés temporairement sur le disque dur de l’ordinateur de l’Utilisateur. Ces cookies sont nécessaires pour assurer l’accessibilité et la navigation sur le site. Ces fichiers ne comportent pas d’informations personnelles et ne peuvent pas être utilisés pour l’identification d’une personne.
+                    L’information présente dans les cookies est utilisée pour améliorer les performances de navigation sur le site lapetiteperle.fr.
+                    En naviguant sur le site, l’Utilisateur accepte les cookies. Leur désactivation peut s’effectuer via les paramètres du logiciel de navigation.
+                </p>
+            </article>
+
+            <article>
+                <h3>Article 9 : Publication par l’Utilisateur</h3>
+                <p>
+                    Toukki permet aux membres de publier des commentaires.
+                    Dans ses publications, le membre est tenu de respecter les règles de la Netiquette ainsi que les règles de droit en vigueur.
+                    L’application dispose du droit d’exercer une modération à priori sur les publications et peut refuser leur mise en ligne sans avoir à fournir de justification.
+                    Le membre garde l’intégralité de ses droits de propriété intellectuelle. Toutefois, toute publication sur le site implique la délégation du droit non exclusif et gratuit à la société éditrice de représenter, reproduire, modifier, adapter, distribuer et diffuser la publication n’importe où et sur n’importe quel support pour la durée de la propriété intellectuelle. Cela peut se faire directement ou par l’intermédiaire d’un tiers autorisé. Cela concerne notamment le droit d’utilisation de la publication sur le web et sur les réseaux de téléphonie mobile.
+                    À chaque utilisation, l’éditeur s’engage à mentionner le nom du membre à proximité de la publication.
+                    L’Utilisateur est tenu responsable de tout contenu qu’il met en ligne. L’Utilisateur s’engage à ne pas publier de contenus susceptibles de porter atteinte aux intérêts de tierces personnes. Toutes procédures engagées en justice par un tiers lésé à l’encontre du site devront être prises en charge par l’Utilisateur.
+                    La suppression ou la modification par le site du contenu de l’Utilisateur peut s’effectuer à tout moment, pour n’importe quelle raison et sans préavis.
+                </p>
+            </article>
+
+            <article>
+                <h3>Article 10 : Durée du contrat</h3>
+                <p>
+                    Le présent contrat est valable pour une durée indéterminée. Le début de l’utilisation des services du site marque l’application du contrat à l’égard de l’Utilisateur.
+                </p>
+            </article>
+
+            <article>
+                <h3>Article 12 : Droit applicable et juridiction compétente</h3>
+                <p>
+                    Le présent contrat est soumis à la législation française. L’absence de résolution à l’amiable des cas de litige entre les parties implique le recours aux tribunaux français compétents pour régler le contentieux.
+                </p>
+            </article>
+
+          </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+<style>
+    h1{
+        color: #df0b13;
+        font-weight: bold;
+    }
+
+    article{
+        padding-top: 20px;
+    }
+
+    .art{
+        margin-bottom: 50px;
+    }
+</style>
